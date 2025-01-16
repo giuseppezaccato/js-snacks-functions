1165b8486448d81a69068f18a8bae31da929949e
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-16 21:53:38 +0100

    ADD: 04 (che sudata!)

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,31 +1,29 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-const word1 = prompt("inserisci qui una parola a tuo piacimento!", "supercalifragilistichespiralidoso")
-const vowels = ["a", "e", "i", "o", "u"];
+//*BONUS const word1 = prompt("inserisci qui una parola a tuo piacimento!", "supercalifragilistichespiralidoso");
 const vowelsIN = [];
-const counter = "";
 
 
 // Dichiara la funzione qui.
 function vocali(parola1, parola2) {
-    for (i = 0; i < parola1.length; i++) {
-        if (parola1[i].split("") == vowels) {
-            parola2.push(parola1.charAt((vowels[i]))).join(",");
+    let counter = 0;
+    const vowels = ["a", "e", "i", "o", "u"];
+
+    for (let i = 0; i < parola1.length; i++) {
+        if (vowels.includes(parola1.charAt(i))) {
+            parola2.push(parola1.charAt(i));
             counter++;
         }
     }
-
-    // if () {
-    //     alert("non sono presenti vocali nella tua parola!")
-    // }
-    return parola2, counter;
+    return counter, parola2;
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(vocali(word, vowelsIN))
+//* BONUS console.log(vocali(word1, vowelsIN))
 
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(vocali(word, vowelsIN));
