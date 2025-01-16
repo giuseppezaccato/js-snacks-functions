efb207a83d08a385fd53beb1183232e3e314e053
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-16 20:11:49 +0100

    ADD: 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,31 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const names2 = [];
+const capital = prompt("aggiungi qui una lettera per sapere se nella lista ci sono persone con la stessa iniziale", "A").toUpperCase();
 
 
 // Dichiara la funzione qui.
+function FirstLetter(array1, array2, letter) {
+    for (i = 0; i < array1.length; i++) {
+        if (letter === array1[i].charAt(0)) {
+            array2.push(array1[i]);
+        }
+    }
 
+    //!come stampare l'else in questo caso fuori dal FOR
+    if (array2 == 0) {
+        alert("la lista è vuota!")
+    } else {
+        return array2;
+    }
+}
+//* tentativo => arrow/ternary "FirstLetter = (array1, array2, letter) =>...
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(FirstLetter(names, names2, capital));
 
 
 
-//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"] (per funzionare commenta il log a 25!!!)
+console.log(FirstLetter(names, names2, "A"));
